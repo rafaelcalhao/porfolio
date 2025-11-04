2a3e9b7839ece8c92a423254d31832e74a5de2a9
Rafael Pinheiro <rafa_calhao@outlook.com>
2025-11-04 13:03:37 -0300

    feat: add new dependencies and configure image provider

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,30 @@
 import type { NextConfig } from "next";
 
+
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+     {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',  // Para URLs diretas de fotos
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'source.unsplash.com',  // Para random
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'plus.unsplash.com',  // Para variações processadas
+        port: '',
+        pathname: '/**',
+      },
+    ] 
+  }
 };
 
 export default nextConfig;
